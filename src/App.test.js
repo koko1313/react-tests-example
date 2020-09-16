dfec66732fda79e0cbf4709b63a88214f302b570
koko1313 <kaloyanvellichkov@gmail.com>
2020-09-16 17:02:54 +0300

    fixing test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders "Hello world!"', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Hello world!/i);
   expect(linkElement).toBeInTheDocument();
 });
